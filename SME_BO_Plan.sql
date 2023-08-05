51cd2740a90d2b93b90a0f0097b94880cfa2c140
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-08-05 13:58:51 +0700

    Update SME_BO_Plan.sql

--- SME_BO_Plan.sql
+++ SME_BO_Plan.sql
@@ -61,14 +61,15 @@ alter table tabSME_BO_and_Plan drop column priority_for_visit;
 -- tabSME_BO_and_Plan -> test SME_BO_and_Plan https://docs.google.com/spreadsheets/d/1dqrXSSNq6ACAKzOuep62Qxl8ebKF9SUI3Z5v6e6f9sE/edit#gid=1867067672
 select bp.modified `Timestamp`, concat('http://13.250.153.252:8000/app/sme_bo_and_plan/', name) `Edit`,
 	sme.dept `DEPT`, sme.sec_branch `SECT`, sme.unit_no `Unit_no`, sme.unit `Unit`,
-	bp.staff_no `Staff No`, bp.staff_name `Staff Name`, `point` , `type` , bp.usd_amount , bp.normal_bullet , bp.contract_no , bp.case_no , bp.customer_name , bp.customer_tel ,
+	bp.staff_no `Staff No`, sme.staff_name `Staff Name`, `point` , `type` , bp.usd_amount , bp.normal_bullet , bp.contract_no , bp.case_no , bp.customer_name , bp.customer_tel ,
 	bp.ringi_status , bp.ringi_comment , bp.disbursement_date_pay_date , bp.contract_status , bp.contract_comment , bp.customer_card , bp.rank1 , bp.approch_list , bp.rank_update , 
 	bp.visit_date , bp.visit_or_not ,
 	case when contract_status = 'Cancelled' then 'Cancelled' else null end `Cancelled Result`, 
 	case when rank_update in ('S','A','B','C') then 1 else 0 end `SABC`, 
 	case when rank_update in ('C') then 1 else 0 end `C`, 
 	sme.sales_cc `sales_cc`, bp.name `id`,
-	concat(bp.staff_no,'-', date_format(bp.visit_date, '%c'),'-',date_format(bp.visit_date, '%e'),'-', case when bp.priority_to_visit = '' then 1 else bp.priority_to_visit end ) `Visit plan M-D-P`
+	concat(bp.staff_no,'-', date_format(bp.visit_date, '%c'),'-',date_format(bp.visit_date, '%e'),'-', case when bp.priority_to_visit = '' then 1 else bp.priority_to_visit end ) `Visit plan M-D-P`,
+	bp.sp_cc `Sales promotion CC`
 	-- concat('=hyperlink(', concat('"http://13.250.153.252:8000/app/sme_bo_and_plan/', name) ,'","', bp.customer_name, '")') `For visit`
 from tabSME_BO_and_Plan bp left join sme_org sme on (bp.staff_no = sme.staff_no)
 order by bp.name asc;
