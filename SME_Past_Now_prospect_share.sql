074ac84c5fc7f9396e582bac621d6d00bd39c4de
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-12-15 13:47:32 +0700

    Update SME_Past_Now_prospect_share.sql

--- SME_Past_Now_prospect_share.sql
+++ SME_Past_Now_prospect_share.sql
@@ -101,12 +101,12 @@ order by case when bp.callcenter_of_sales is null or bp.callcenter_of_sales = ''
 -- SME_past_SABC https://docs.google.com/spreadsheets/d/1ufgINqUYboNJtQLqMFbywADzm_mLQTllh4QtlZODPi0/edit#gid=700696176
 select bp.modified `Timestamp`, concat('http://13.250.153.252:8000/app/sme_bo_and_plan/', name) `Edit`,
 	-- sme.dept `DEPT`, sme.sec_branch `SECT`, sme.unit_no `Unit_no`, sme.unit `Unit`, bp.staff_no `Staff No`, sme.staff_name `Staff Name`, 
-	case when bp.callcenter_of_sales is null or bp.callcenter_of_sales = '' then sme.dept else smec.dept end `DEPT`, 
-	case when bp.callcenter_of_sales is null or bp.callcenter_of_sales = '' then sme.sec_branch else smec.sec_branch end `SECT`, 
-	case when bp.callcenter_of_sales is null or bp.callcenter_of_sales = '' then sme.unit_no else smec.unit_no end `Unit_no`, 
-	case when bp.callcenter_of_sales is null or bp.callcenter_of_sales = '' then sme.unit else smec.unit end `Unit`, 
-	case when bp.callcenter_of_sales is null or bp.callcenter_of_sales = '' then bp.staff_no else regexp_replace(bp.callcenter_of_sales  , '[^[:digit:]]', '') end `Staff No`, 
-	case when bp.callcenter_of_sales is null or bp.callcenter_of_sales = '' then sme.staff_name else smec.staff_name end `Staff Name`, 
+	sme.dept `DEPT`, 
+	sme.sec_branch `SECT`, 
+	sme.unit_no `Unit_no`, 
+	sme.unit `Unit`, 
+	bp.staff_no `Staff No`, 
+	sme.staff_name `Staff Name`, 
 	1 `point`, `type`, bp.usd_loan_amount, 
 	bp.normal_bullet , bp.contract_no , bp.case_no , bp.customer_name , bp.customer_tel ,
 	bp.ringi_status , bp.ringi_comment , bp.disbursement_date_pay_date , bp.contract_status , bp.contract_comment , bp.customer_card , bp.rank1 , bp.approch_list , bp.rank_update , 
@@ -166,13 +166,15 @@ select bp.modified `Timestamp`, concat('http://13.250.153.252:8000/app/sme_bo_an
 	end `visit_order`,
 	left(bp.address_province_and_city, locate('-', bp.address_province_and_city)-2) `province`, 
 	replace(bp.address_province_and_city, left(bp.address_province_and_city, locate('-', bp.address_province_and_city)+1), '')  `district`, bp.address_village,
-	is_sales_partner `SP_rank`
+	is_sales_partner `SP_rank`,
+	bp.disbursement_date_pay_date , 'ແຜນເພີ່ມ' `which`, bp.name, bp.credit, 
+	case when locate(' ', bp.rank_of_credit) = 0 then bp.rank_of_credit else left(bp.rank_of_credit, locate(' ', bp.rank_of_credit)-1) end `rank_of_credit`,
+	case when bp.credit_remark is not null then bp.credit_remark else bp.contract_comment end `comments`
 	-- concat('=hyperlink(', concat('"http://13.250.153.252:8000/app/sme_bo_and_plan/', name) ,'","', bp.customer_name, '")') `For visit`
 from tabSME_BO_and_Plan bp left join sme_org sme on (bp.staff_no = sme.staff_no)
-left join sme_org smec on (regexp_replace(bp.callcenter_of_sales  , '[^[:digit:]]', '') = smec.staff_no)
--- where bp.`custtbl_id` is not null and bp.creation >= '2022-10-01'
-where bp.name in (select id from temp_sme_SABCF where `type` in ('SABC', 'S', 'A', 'B', 'C') )
-order by case when bp.callcenter_of_sales is null or bp.callcenter_of_sales = '' then sme.unit_no else smec.unit_no end asc;
+-- left join sme_org smec on (regexp_replace(bp.callcenter_of_sales  , '[^[:digit:]]', '') = smec.staff_no)
+where bp.name in (select id from temp_sme_SABCF where `type` in ('SABC', 'S', 'A', 'B', 'C') and month_type <= 36)
+order by sme.id asc;
 
 
 
