9cd94754e9f3294708f89a2c9ce1ef3ec3132f9e
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-12-21 15:52:33 +0700

    Update temp_PBX.sql

--- temp_PBX.sql
+++ temp_PBX.sql
@@ -10,7 +10,14 @@ create table `temp_sme_BO_pbx` (
 )engine=InnoDB auto_increment=1 default CHARSET=utf8mb3 collate=utf8mb3_general_ci;
 
 
-
+-- 2) create temp_sme_SP_pbx for import the data call on pbx to check did they call by IP Phone or not
+create table `temp_sme_SP_pbx` (
+	`id` int(11) not null auto_increment,
+	`broker_tel` varchar(255) default null,
+	`pbx_status` varchar(255) default null,
+	`date` datetime default null,
+	primary key (`id`)
+)engine=InnoDB auto_increment=1 default CHARSET=utf8mb3 collate=utf8mb3_general_ci;
 
 
 
