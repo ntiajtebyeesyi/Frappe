2d134d9afbcc673614bea91fa1c5a4d244cd7016
Khamvang <108178100+Khamvang@users.noreply.github.com>
2024-01-06 21:19:34 +0700

    Update SME_Past_Now_prospect_calculate.sql

--- SME_Past_Now_prospect_calculate.sql
+++ SME_Past_Now_prospect_calculate.sql
@@ -23,8 +23,28 @@ where bp.name in (select id from temp_sme_SABCF where `type` in ('SABC', 'S', 'A
 order by sme.id asc;
 
 
+insert into tabSME_BO_and_Plan; select * from tabSME_BO_and_Plan_bk where name not in (select name from tabSME_BO_and_Plan);
 
--- F_3mo result
+select * from tabsme_Sales_partner_bk where name not in (select name from tabsme_Sales_partner);
+
+-- check and add to the list temp_sme_pbx_BO
+select bp.name `id`, bp.customer_tel `tel`, null `pbx_status`, null `date`, sme.staff_no `current_staff`, 
+	case when bp.contract_status = 'Contracted' then 'Contracted' when bp.contract_status = 'Cancelled' then 'Cancelled' else bp.rank_update end `type`,
+	timestampdiff(month,date_format(bp.creation, '%Y-%m-%d'), date(now())) `month_type`
+from tabSME_BO_and_Plan bp left join sme_org sme on (case when locate(' ', bp.staff_no) = 0 then bp.staff_no else left(bp.staff_no, locate(' ', bp.staff_no)-1) end = sme.staff_no)
+left join sme_org smec on (regexp_replace(bp.callcenter_of_sales  , '[^[:digit:]]', '') = smec.staff_no)
+where date_format(bp.creation, '%Y-%m-%d') >= '2023-11-01' and bp.rank_update in ('S', 'A', 'B', 'C') 
+	and bp.contract_status not in ('Contracted', 'Cancelled')
+order by bp.name asc;
+
+-- updtate assign to new people
+update tabSME_BO_and_Plan bp inner join temp_sme_pbx_BO tss on (bp.name = tss.id)
+set bp.staff_no = tss.current_staff
+where tss.`type` in ('SABC', 'S', 'A', 'B', 'C') ;
+
+
+
+-- __________________________________________ F_3mo result __________________________________________
 select bp.modified `Timestamp`, 
 	bp.name `id`,
 	sme.unit_no `Unit_no`, 
