000c90f999ff1d9af646233bbafdbba1007fa156
Khamvang <108178100+Khamvang@users.noreply.github.com>
2024-01-26 07:21:37 +0700

    Update sme_Sales_partner.sql

--- sme_Sales_partner.sql
+++ sme_Sales_partner.sql
@@ -62,12 +62,26 @@ inner join temp_sme_pbx_SP ts on (ts.id = sp.name)
 where sp.refer_type = 'LMS_Broker' order by sme.id ;
 
 
--- 5way export to google sheet https://docs.google.com/spreadsheets/d/15wAmhxB0gDAHDwa6WSmY-PqPvAm6eOoIP5YKp48wTi4/edit#gid=406704453
+-- export to google sheet https://docs.google.com/spreadsheets/d/19IsoiG6JyB1CNodTyDLvLeb7HUM3CYnqHOs7XhjNErE/edit#gid=990597291
 select sp.name `id`, date_format(sp.modified, '%Y-%m-%d') `date_update`, sme.`dept`, sme.`sec_branch`, sme.`unit_no`, sme.unit, sp.current_staff `staff_no` , sme.staff_name, sp.owner_staff 'owner', 
 	sp.broker_type, sp.broker_name, sp.address_province_and_city, sp.`rank`, sp.date_for_introduction, sp.customer_name, concat('http://13.250.153.252:8000/app/sme_sales_partner/', sp.name) `Edit`,
 	case when sp.owner_staff = sp.current_staff then '1' else 0 end `owner_takeover`,
-	sp.broker_tel
+	sp.broker_tel, sp.currency, sp.amount,
+	ts.pbx_status `LCC check`
 from tabsme_Sales_partner sp left join sme_org sme on (sp.current_staff = sme.staff_no)
+inner join temp_sme_pbx_SP ts on (ts.id = sp.name)
+where sp.refer_type = 'tabSME_BO_and_Plan' order by sme.id ;
+
+
+-- 5way export to google sheet https://docs.google.com/spreadsheets/d/15wAmhxB0gDAHDwa6WSmY-PqPvAm6eOoIP5YKp48wTi4/edit#gid=722440269
+select sp.name `id`, date_format(sp.modified, '%Y-%m-%d') `date_update`, sme.`dept`, sme.`sec_branch`, sme.`unit_no`, sme.unit, sp.current_staff `staff_no` , sme.staff_name, sp.owner_staff 'owner', 
+	sp.broker_type, sp.broker_name, sp.address_province_and_city, sp.`rank`, sp.date_for_introduction, sp.customer_name, concat('http://13.250.153.252:8000/app/sme_sales_partner/', sp.name) `Edit`,
+	case when sp.owner_staff = sp.current_staff then '1' else 0 end `owner_takeover`,
+	sp.broker_tel, sp.currency, sp.amount,
+	ts.pbx_status `LCC check`,
+	sp.business_type
+from tabsme_Sales_partner sp left join sme_org sme on (sp.current_staff = sme.staff_no)
+inner join temp_sme_pbx_SP ts on (ts.id = sp.name)
 where sp.broker_type = '5way - 5ສາຍພົວພັນ' and sp.owner_staff = sp.current_staff order by sme.id ;
 
 
